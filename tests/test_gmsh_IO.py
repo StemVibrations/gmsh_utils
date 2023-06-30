e6f98edb4459f752c0dc40d9156d45214f843180
Marjan Fathian <65416980+indigocoral@users.noreply.github.com>
2023-06-30 12:15:54 +0200

    Updated tests for checking physical groups

--- tests/test_gmsh_IO.py
+++ tests/test_gmsh_IO.py
@@ -321,28 +321,21 @@ class TestGmshIO:
 
     def test_geometry_data_2D(self):
         """
-        Checks whether mesh data generated for 2D geometries is not empty.
+        Checks whether geometry data in 2D geometry has physical groups
     """
         # define the default mesh size
         default_mesh_size = -1
         # define the points of the surface as a list of tuples
         input_points_list = [[(0, 0, 0), (3, 0, 0), (3, 1, 0), (0, 1, 0)],
-                             [(3, 0, 0), (5, 0, 0), (5, 1, 0), (4, 1.5, 0), (3, 1, 0)],
-                             [(0, 1, 0), (2, 1, 0), (2, 3, 0), (0, 3, 0)],
-                             [(2, 1, 0), (3, 1, 0), (4, 1.5, 0), (5, 1, 0), (5, 3, 0), (2, 3, 0)],
-                             [(0, 3, 0), (2.5, 3, 0), (2, 4, 0), (0, 4, 0)],
-                             [(0.8, 4, 0), (1.2, 4, 0), (1.2, 4.1, 0), (0.8, 4.1, 0)]]
+                             [(0, 1, 0), (3, 1, 0), (3, 2, 0), (0, 2, 0)],
+                             [(1, 2, 0), (2, 2, 0), (2, 2.5, 0), (1, 2.5, 0)]]
         # define the name labels for the surfaces
-        name_label_list = ["First Soil Layer", "FSL", "Second Soil Layer", "SSL", "Soil Ballast", "Line Track"]
+        name_label_list = ["Soil Layer", "Soil Embankment", "Soil Ballast"]
 
         # define geometry dimension; input "3" for 3D to extrude the 2D surface, input "2" for 2D
         dims = 2
         # if 3D, input depth of geometry to be extruded from 2D surface
         extrusion_length = [0, 0, 0]
-        # if "True", saves mesh data to separate mdpa files; otherwise "False"
-        save_file = False
-        # if "True", opens gmsh interface; otherwise "False"
-        open_gmsh_gui = False
         # set a name for mesh output file
         mesh_output_name = "test_2D"
         # set output directory
@@ -352,18 +345,19 @@ class TestGmshIO:
 
         gmsh_io.generate_geometry(input_points_list, extrusion_length, dims,
                                   mesh_output_name, name_label_list, default_mesh_size)
-        gmsh_io.generate_extract_mesh(dims, mesh_output_name, mesh_output_dir, save_file, open_gmsh_gui)
 
         geo_data = gmsh_io.geo_data
 
-        assert geo_data is not None
+        expected_physical_groups = {'Soil Layer': {'ndim': 2, 'id': 1, 'geometry_id': 1},
+                                    'Soil Embankment': {'ndim': 2, 'id': 2, 'geometry_id': 2},
+                                    'Soil Ballast': {'ndim': 2, 'id': 3, 'geometry_id': 3}}
 
-        for i in geo_data["physical_groups"].keys():
-            assert geo_data["physical_groups"][i] is not None
+        # check if expected and actual geo data are equal
+        TestUtils.assert_dictionary_almost_equal(expected_physical_groups, geo_data["physical_groups"])
 
     def test_geometry_data_3D(self):
         """
-        Checks whether mesh data generated for 3D geometries is not empty.
+        Checks whether geometry data in 3D geometry has physical groups
 
         """
 
@@ -371,22 +365,15 @@ class TestGmshIO:
         default_mesh_size = 1
         # define the points of the surface as a list of tuples
         input_points_list = [[(0, 0, 0), (3, 0, 0), (3, 1, 0), (0, 1, 0)],
-                             [(3, 0, 0), (5, 0, 0), (5, 1, 0), (4, 1.5, 0), (3, 1, 0)],
-                             [(0, 1, 0), (2, 1, 0), (2, 3, 0), (0, 3, 0)],
-                             [(2, 1, 0), (3, 1, 0), (4, 1.5, 0), (5, 1, 0), (5, 3, 0), (2, 3, 0)],
-                             [(0, 3, 0), (2.5, 3, 0), (2, 4, 0), (0, 4, 0)],
-                             [(0.8, 4, 0), (1.2, 4, 0), (1.2, 4.1, 0), (0.8, 4.1, 0)]]
+                             [(0, 1, 0), (3, 1, 0), (3, 2, 0), (0, 2, 0)],
+                             [(1, 2, 0), (2, 2, 0), (2, 2.5, 0), (1, 2.5, 0)]]
         # define the name labels for the surfaces
-        name_label_list = ["First Soil Layer", "FSL", "Second Soil Layer", "SSL", "Soil Ballast", "Line Track"]
+        name_label_list = ["Soil Layer", "Soil Embankment", "Soil Ballast"]
 
         # define geometry dimension; input "3" for 3D to extrude the 2D surface, input "2" for 2D
         dims = 3
         # if 3D, input depth of geometry to be extruded from 2D surface
         extrusion_length = [0, 0, 3]
-        # if "True", saves mesh data to separate mdpa files; otherwise "False"
-        save_file = False
-        # if "True", opens gmsh interface; otherwise "False"
-        open_gmsh_gui = False
         # set a name for mesh output file
         mesh_output_name = "test_3D"
         # set output directory
@@ -396,11 +383,12 @@ class TestGmshIO:
 
         gmsh_io.generate_geometry(input_points_list, extrusion_length, dims,
                                   mesh_output_name, name_label_list, default_mesh_size)
-        gmsh_io.generate_extract_mesh(dims, mesh_output_name, mesh_output_dir, save_file, open_gmsh_gui)
 
         geo_data = gmsh_io.geo_data
 
-        assert geo_data is not None
+        expected_physical_groups = {'Soil Layer': {'ndim': 3, 'id': 1, 'geometry_id': 1},
+                                    'Soil Embankment': {'ndim': 3, 'id': 2, 'geometry_id': 2},
+                                    'Soil Ballast': {'ndim': 3, 'id': 3, 'geometry_id': 3}}
 
-        for i in geo_data["physical_groups"].keys():
-            assert geo_data["physical_groups"][i] is not None
+        # check if expected and actual geo data are equal
+        TestUtils.assert_dictionary_almost_equal(expected_physical_groups, geo_data["physical_groups"])
